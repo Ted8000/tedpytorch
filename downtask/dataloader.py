5a8eb2671f662c0e863b8101244aad195ccecf92
ted3000 <17628292357@163.com>
2023-03-23 03:10:28 +0000

    u

--- downtask/dataloader.py
+++ downtask/dataloader.py
@@ -4,15 +4,15 @@ from sklearn.preprocessing import LabelEncoder
 from torch.utils.data import dataloader, Dataset, dataset, DataLoader
 import pandas as pd
 from sklearn.utils import shuffle
-import time
-import pickle
 import logging
 
+logging.basicConfig(format='%(asctime)s - %(message)s', filename='log', filemode='a', level=logging.DEBUG)
+
 class CustomDataset(Dataset):
-    def __init__(self, df, label2id, length=32):
+    def __init__(self, df, label2id, length, tok):
         self.df = df
         self.length = length
-        self.tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
+        self.tokenizer = AutoTokenizer.from_pretrained(tok)
         self.train_x = self.df.iloc[:,0].tolist()
         self.train_y = list(map(lambda x:label2id[x], self.df.iloc[:,1].tolist()))
         
@@ -25,8 +25,8 @@ class CustomDataset(Dataset):
         batch['label'] = torch.tensor(self.train_y[index])
         return batch
 
-def parse_data(path, batch_size, length):
-    df = pd.read_csv(path, sep='\t')
+def parse_data(config):
+    df = pd.read_csv(config.path, sep='\t')
     label_names = df.columns.values.tolist()[1:]
     
     num=int(len(df)*0.8)
@@ -44,12 +44,12 @@ def parse_data(path, batch_size, length):
         id2label[i]=x
         label2id[x]=i
     
-    train_set = CustomDataset(df_train, label2id, length)
-    dev_set = CustomDataset(df_dev, label2id, length)
-    test_set = CustomDataset(df_test, label2id, length)
+    train_set = CustomDataset(df_train, label2id, config.length, config.tokenizer)
+    dev_set = CustomDataset(df_dev, label2id, config.length, config.tokenizer)
+    test_set = CustomDataset(df_test, label2id, config.length, config.tokenizer)
     
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=4)
-    dev_loader = DataLoader(dev_set, batch_size=batch_size, shuffle=True, num_workers=4)
-    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers=4)
+    train_loader = DataLoader(train_set, batch_size=config.batch_size, shuffle=True, num_workers=4)
+    dev_loader = DataLoader(dev_set, batch_size=config.batch_size, shuffle=True, num_workers=4)
+    test_loader = DataLoader(test_set, batch_size=config.batch_size, shuffle=True, num_workers=4)
     
     return train_loader, dev_loader, test_loader, id2label, label2id
---
+++ downtask/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python main.py  --df_file data/df_train.csv \
+                --epochs 1 \
+                --path ~/data/models/small
